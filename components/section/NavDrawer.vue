397057d14a30a7edfad9d141d655dc90d797083b
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-07 22:24:50 +0700

    feat: Nav Drawer

---
+++ components/section/NavDrawer.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-navigation-drawer
+    app
+    permanent
+    :rail="rail"
+    class="pl-2"
+    floating
+  >
+    <!-- Logo -->
+    <v-list-item
+      title="Berry"
+      density="compact"
+      height="80"
+      class="px-1"
+      slim
+    >
+      <template #prepend>
+        <v-icon
+          size="40"
+          color="secondary"
+          class="rounded-circle"
+          style="opacity: 1 !important;"
+        >mdi-fruit-grapes-outline</v-icon>
+      </template>
+    </v-list-item>
+
+    <v-list
+      v-for="(menu, index) in NAV_MENU"
+      :key="index"
+      slim
+    >
+      <v-list-subheader v-if="menu.head" :key="menu.head" class="text-subtitle-2 font-weight-bold text-black px-0">{{ menu.head }}</v-list-subheader>
+      <template
+        v-for="(item, idx) in menu.items"
+      >
+        <v-list-item
+          v-if="!item.subItems"
+          :key="idx"
+          :title="item.title"
+          :prepend-icon="item.icon"
+          :value="item.title"
+          color="secondary"
+          density="compact"
+          nav
+        />
+
+        <!-- Sub Items-->
+        <v-list-group
+          v-if="item.subItems"
+          :value="item.title"
+        >
+          <template v-slot:activator="{ props }">
+            <v-list-item
+              v-bind="props"
+              :title="item.title"
+              :value="item.title"
+              :prepend-icon="item.icon"
+              nav density="compact"
+              color="secondary"
+            ></v-list-item>
+          </template>
+
+          <v-list-item
+            v-for="(subMenu, i) in item.subItems"
+            :key="i"
+            :title="subMenu.title"
+            :value="subMenu.title"
+            prepend-icon="mdi-circle-small"
+            density="compact" nav
+            color="secondary"
+          >
+            
+          </v-list-item>
+        </v-list-group>
+      </template>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script setup lang="ts">
+import { NAV_MENU } from '@/config/nav-menu';
+
+const rail = defineModel('rail', {
+  type: Boolean,
+  default: false,
+});
+</script>
---
+++ config/nav-menu.ts
@@ -0,0 +1,102 @@
+export const NAV_MENU = [
+  {
+    head: 'Dashboard',
+    items: [
+      {
+        title: 'Default',
+        icon: 'mdi-view-dashboard',
+      },
+      {
+        title: 'Analytics',
+        icon: 'mdi-chart-bar',
+      },
+    ]
+  },
+  {
+    head: 'Applications',
+    items: [
+      {
+        title: 'Users',
+        icon: 'mdi-account',
+        subItems: [
+          {
+            title: 'Social Profile',
+          },
+          {
+            title: 'Accounts',
+          },
+          {
+            title: 'Cards',
+          },
+          {
+            title: 'List',
+          },
+        ]
+      },
+      {
+        title: 'Customers',
+        icon: 'mdi-account-multiple',
+        subItems: [
+          {
+            title: 'Customer List',
+          },
+          {
+            title: 'Create Invoice',
+          },
+          {
+            title: 'Order Details',
+          },
+          {
+            title: 'Product List',
+          },
+        ]
+      },
+      {
+        title: 'Chat',
+        icon: 'mdi-chat',
+      },
+      {
+        title: 'Calendar',
+        icon: 'mdi-calendar',
+      },
+      {
+        title: 'Mail',
+        icon: 'mdi-email',
+      },
+      {
+        title: 'Kanban',
+        icon: 'mdi-view-dashboard-variant-outline',
+      },
+      {
+        title: 'Contact',
+        icon: 'mdi-card-account-details-outline',
+        subItems: [
+          {
+            title: 'Card',
+          },
+          {
+            title: 'List',
+          },
+        ]
+      },
+      {
+        title: 'E-Commerce',
+        icon: 'mdi-cart',
+        subItems: [
+          {
+            title: 'Products',
+          },
+          {
+            title: 'Product Details',
+          },
+          {
+            title: 'Product List',
+          },
+          {
+            title: 'Checkout',
+          },
+        ]
+      }
+    ]
+  }
+];
