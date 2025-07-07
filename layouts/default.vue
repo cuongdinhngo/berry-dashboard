9e53b6c4748b4949e9ee5aab5d679573d7378a26
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-07 22:24:28 +0700

    feat: default layout

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <!-- Navigation Drawer -->
+    <SectionNavDrawer
+      v-model:rail="rail"
+    />
+
+    <!-- App Bar -->
+    <SectionAppBar
+      v-model:rail="rail"
+    />
+
+    <!-- Main Content -->
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app>
+</template>
+<script setup lang="ts">
+import { NAV_MENU } from '@/config/nav-menu';
+const rail = ref(false);
+</script>
