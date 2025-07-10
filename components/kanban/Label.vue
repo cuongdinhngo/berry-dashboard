b413787fb2959718ec5159288372ae6a852b23d8
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-10 22:53:38 +0700

    feat: Label component

---
+++ components/kanban/Label.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="task-label" :style="`background-color: ${color};`">
+    <v-tooltip
+      activator="parent"
+      location="top"
+    >{{ label }}</v-tooltip>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    default: 'green',
+    required: true,
+  }
+});
+</script>
+<style scoped>
+.task-label {
+  width: 40px;
+  height: 7px;
+  border-radius: 4px;
+}
+</style>
